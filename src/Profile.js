1bf75e202118b9960c7a50dd2b69426fe04613ee
Pierre Faniel <13941041+PierreFaniel@users.noreply.github.com>
2024-05-24 13:25:31 -0400

    Add social in profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './Profile.css';
-import profile from './profile.webp';
+import profile_picture from './profile.webp';
+import gitlab_logo from './logos/gitlab.png'
+import github_logo from './logos/github.png'
+import linkedin_logo from './logos/linkedin.png'
+import twitter_logo from './logos/twitter.png'
 
 class Profile extends Component {
     computeAge(){
@@ -26,6 +30,12 @@ class Profile extends Component {
                             <strong>Age:</strong>
                             <div>{this.computeAge()} years</div>
                         </div>
+                        <div className="social-logos">
+                            <a href="https://gitlab.com/PierreFaniel"><img src={gitlab_logo} alt="Pierre Faniel on GitLab" title="Check me on GitLab"/></a>
+                            <a href="https://github.com/PierreFaniel"><img src={github_logo} alt="Pierre Faniel on GitHub" title="Check me on GitHub"/></a>
+                            <a href="https://linkedin.com/in/PierreFaniel"><img src={linkedin_logo} alt="Pierre Faniel on LinkedIn" title="Check me on LinkedIn"/></a>
+                            <a href="https://twitter.com/PierreFaniel"><img src={twitter_logo} alt="Pierre Faniel on Twitter" title="Check me on Twitter"/></a>
+                        </div>
                     </div>
                     <div className="profile-description">
                         Since starting as a full time developer in 2015, I have been working with <a href="https://www.odoo.com/">Odoo</a>, a Belgium made open source ERP software.
@@ -34,7 +44,7 @@ class Profile extends Component {
                         I am detail oriented with a particular affection for state of the art software. I also would love to discover a whole new stack!
                     </div>
                     <div className="profile-picture">
-                        <img src={profile} alt="Pierre Faniel profile - 2017"/>
+                        <img src={profile_picture} alt="Pierre Faniel profile - 2017"/>
                     </div>
                 </div>
             </div>
