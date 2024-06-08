ce0b24d451d8afcc1e50396e6a0f3f2e0d98f091
Pierre Faniel <13941041+PierreFaniel@users.noreply.github.com>
2024-06-08 12:36:25 -0400

    Add re usable componenent for experience item

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,163 +1,158 @@
 import { Component } from "react";
-import "@/components/Experience.css";
+import ExperienceItem from "@/components/ExperienceItem";
 
-export default class Experiences extends Component {
+export default class Experience extends Component {
   render() {
     return (
       <section id="experienceSection" className="container experience">
         <h2>Work Experience</h2>
-        <div className="experience-content">
-          <div className="experience-item">
-            <div className="left-column">08/2018 - 05/2019</div>
-            <div className="right-column">
-              <strong>Senior Full Stack Developer</strong>
-              <span className="badge badge-orange">full-time</span>
-              <br />
-              <i>
-                <a
-                  className="company-link"
-                  href="https://www.niboo.com/"
-                  target="_blank"
-                >
-                  Niboo, Brussels, Belgium
-                </a>
-              </i>
-              <div className="job-description">
-                After a short tenure at Stanler/Stella, Pierre came back to
-                Niboo as a technical team leader. He managed a scrum team of 5
-                developers. He lead and participated in several Odoo
-                implementation projects. He also trained developers with less
-                experience.
-                <ul className="tasks-description">
-                  <li>
-                    Improved performances on complex Python background jobs
-                    reducing execution time from 2+ hours to under 3 minutes.
-                  </li>
-                  <li>
-                    Integrated tailored solutions into Odoo ERP for 20+ clients
-                  </li>
-                  <li>
-                    Drove dozens of technical trainings internally for less
-                    experienced developers (e.g. Odoo ERP basics, Git flow,
-                    accounting with Odoo)
-                  </li>
-                </ul>
-              </div>
-              <div className="skills-used">
-                <i>Skills:</i> Odoo | Scrum | Python 2.7 | Celery | jQuery |
-                OdooJs | HTML + CSS | PostrgeSQL | Docker | Bash
-              </div>
-            </div>
-          </div>
-          <div className="experience-item">
-            <div className="left-column">04/2018 - 07/2018</div>
-            <div className="right-column">
-              <strong>Full Stack Developer</strong>
-              <span className="badge badge-orange">full-time</span>
-              <br />
-              <i>
-                <a
-                  className="company-link"
-                  href="https://stanleystella.com/"
-                  target="_blank"
-                >
-                  Stanley/Stella, Brussels, Belgium
-                </a>
-              </i>
-              <div className="job-description">
-                As the sole in-house developer of Stanley/Stella, Pierre's main
-                focuses were to improve the existing e-commerce website and ease
-                the life of the company's other departments:
-                <ul className="tasks-description">
-                  <li>
-                    Revamped of the e-commerce platform's responsivness,
-                    implementation of the designer's HTML mock ups
-                  </li>
-                  <li>
-                    Update the physical clothing store's kiosk, which required
-                    learning Electron Js (react) autonomously
-                  </li>
-                  <li>
-                    Implemented a process of one-pager PDF product sheets
-                    generation based on the products' data from the ERP
-                  </li>
-                </ul>
-              </div>
-              <div className="skills-used">
-                <i>Skills:</i> Odoo | Python 2.7 | jQuery | OdooJs | HTML + CSS
-                | PostrgeSQL | Electron JS | React
-              </div>
-            </div>
-          </div>
-          <div className="experience-item">
-            <div className="left-column">08/2015 - 04/2018</div>
-            <div className="right-column">
-              <strong>Full Stack Developer</strong>
-              <span className="badge badge-orange">full-time</span>
-              <br />
-              <i>
-                <a
-                  className="company-link"
-                  href="https://www.niboo.com/"
-                  target="_blank"
-                >
-                  Niboo, Brussels, Belgium
-                </a>
-              </i>
-              <div className="job-description">
-                Pierre was the first full-time employee and developer of Niboo.
-                As a junior develeper out of college, he was a full time Odoo
-                consultant on a yearlong e-commerce project. He then was an Odoo
-                consultant on several smaller Odoo implementation and
-                integration projects simultaneously.
-                <ul className="tasks-description">
-                  <li>
-                    Fully implemented a consultant placement platform built on
-                    top of the Odoo ERP in a month time
-                  </li>
-                  <li>
-                    Conducted multiple user trainings on the whole sales
-                    workflow with Odoo, from marketing to billing
-                  </li>
-                  <li>
-                    Succesfully formatted and imported 10+ new clients'
-                    production data into the new Odoo ERP instance
-                  </li>
-                </ul>
-              </div>
-              <div className="skills-used">
-                <i>Skills:</i> Odoo | Python 2.7 | jQuery | OdooJs | HTML + CSS
-                | PostrgeSQL | Bash
-              </div>
-            </div>
-          </div>
-          <div className="experience-item">
-            <div className="left-column">02/2015 - 05/2015</div>
-            <div className="right-column">
-              <strong>IT Intern</strong>
-              <span className="badge badge-blue">internship</span>
-              <br />
-              <i>
-                <a
-                  className="company-link"
-                  href="https://www.proximus.be/"
-                  target="_blank"
-                >
-                  Proximus, Liège, Belgium
-                </a>
-              </i>
-              <div className="job-description">
-                Implementation from the ground up of a custom Excel report
-                generating tool in VBA for the outsourcing division of the Liège
-                province, aggregating data from 3 databases
-                <ul className="tasks-description" />
-              </div>
-              <div className="skills-used">
-                <i>Skills:</i> VBA | Access SQL | Access | Excel
-              </div>
-            </div>
-          </div>
-        </div>
+        <ExperienceItem
+          badgeColor="orange"
+          company="AlayaCare"
+          companyLink="https://alayacare.ca/"
+          employementDescription="full-time"
+          endDate="12/2023"
+          jobDescription="TODO"
+          jobHighlights={[]}
+          jobTitle="Senior Full Stack Developer"
+          location="Montréal, Canada"
+          startDate="10/2019"
+          skills={[
+            "Python 2 and 3: Flask",
+            "JavaScript: VueJs, Jest, ES6",
+            "PostgreSQL",
+            "Docker",
+            "CI/CD",
+            "AWS",
+            "IAM",
+            "Terraform",
+          ]}
+        />
+        <ExperienceItem
+          badgeColor="orange"
+          company="Microcom"
+          companyLink="https://microcom.ca/"
+          employementDescription="full-time"
+          endDate="10/2019"
+          jobDescription="After reloctating to Canada, Pierre took a small tenure with Microcom as an Odoo specialist. There he worked on several Odoo projects as well as helping with the maintenance with miscelaneous IT projects."
+          jobHighlights={[
+            "Self-directed learning of Angular and MongoDb for maintenance and improvement of a CRM application",
+            "Maintained and implemented custom solution integrated with Odoo ERP",
+          ]}
+          jobTitle="Senior Full Stack Developer"
+          location="Montréal, Canada"
+          startDate="08/2019"
+          skills={[
+            "Odoo",
+            "Python 2.7",
+            "jQuery",
+            "OdooJs",
+            "HTML + CSS",
+            "PostrgeSQL",
+            "Bash",
+            "AngularJS",
+            "MongoDB",
+          ]}
+        />
+        <ExperienceItem
+          badgeColor="grey"
+          employementDescription="pause"
+          endDate="08/2019"
+          jobTitle="Reolcation to Canada"
+          location="Montréal, Québec, Canada"
+          startDate="06/2019"
+        />
+        <ExperienceItem
+          badgeColor="orange"
+          company="Niboo"
+          companyLink="https://www.niboo.com/"
+          employementDescription="full-time"
+          endDate="05/2019"
+          jobDescription="After a short tenure at Stanler/Stella, Pierre came back to Niboo as a technical team leader. He managed a scrum team of 5 developers.  He lead and participated in several Odoo implementation projects. He also  trained developers with less experience."
+          jobHighlights={[
+            "Improved performances on complex Python background jobs reducing execution time from 2+ hours to under 3 minutes.",
+            "Integrated tailored solutions into Odoo ERP for 20+ clients",
+            "Drove dozens of technical trainings internally for less experienced developers (e.g. Odoo ERP basics, Git flow, accounting with Odoo)",
+          ]}
+          jobTitle="Senior Full Stack Developer"
+          location="Brussels, Belgium"
+          startDate="08/2018"
+          skills={[
+            "Odoo",
+            "Scrum",
+            "Python 2.7",
+            "Celery",
+            "jQuery",
+            "OdooJs",
+            "HTML + CSS",
+            "PostrgeSQL",
+            "Docker",
+            "Bash",
+          ]}
+        />
+        <ExperienceItem
+          badgeColor="orange"
+          company="Stanley/Stella"
+          companyLink="https://www.stanleystella.com/"
+          employementDescription="full-time"
+          startDate="04/2018"
+          jobDescription="As the sole in-house developer of Stanley/Stella, Pierre's main focuses were to improve the existing e-commerce website and ease the life of the company's other departments:"
+          jobHighlights={[
+            "Revamped of the e-commerce platform's responsivness, implementation of the designer's HTML mock ups.",
+            "Update the physical clothing store's kiosk, which required learning Electron Js (react) autonomously",
+            "Implemented a process of one-pager PDF product sheets generation based on the products' data from the ERP",
+          ]}
+          jobTitle="Full Stack Developer"
+          location="Brussels, Belgium"
+          endDate="08/2018"
+          skills={[
+            "Odoo",
+            "Python 2.7",
+            "jQuery",
+            "OdooJs",
+            "HTML + CSS",
+            "PostrgeSQL",
+            "Electron JS",
+            "React",
+          ]}
+        />
+        <ExperienceItem
+          badgeColor="orange"
+          company="Niboo"
+          companyLink="https://www.niboo.com/"
+          employementDescription="full-time"
+          endDate="04/2018"
+          jobDescription="Pierre was the first full-time employee and developer of Niboo. As a junior develeper out of college, he was a full time Odoo consultant on a yearlong e-commerce project. He then was an Odoo consultant on several smaller Odoo implementation and integration projects simultaneously."
+          jobHighlights={[
+            "Fully implemented a consultant placement platform built on top of the Odoo ERP in a month time",
+            "Conducted multiple user trainings on the whole sales workflow with Odoo, from marketing to billing",
+            "Succesfully formatted and imported 10+ new clients' production data into the new Odoo ERP instance",
+          ]}
+          jobTitle="Full Stack Developer"
+          location="Brussels, Belgium"
+          startDate="08/2015"
+          skills={[
+            "Odoo",
+            "Python 2.7",
+            "jQuery",
+            "OdooJs",
+            "HTML + CSS",
+            "PostrgeSQL",
+            "Bash",
+          ]}
+        />
+        <ExperienceItem
+          badgeColor="blue"
+          company="Proximus"
+          companyLink="https://www.proximus.be/"
+          employementDescription="internship"
+          endDate="05/2015"
+          jobDescription="Implementation from the ground up of a custom Excel report generating tool in VBA for the outsourcing division of the Liège province, aggregating data from 3 databases"
+          jobTitle="Software Development Intern"
+          location="Liège, Belgium"
+          startDate="01/2015"
+          skills={["VBA", "Access SQL", "MS Access", "MS Excel"]}
+        />
       </section>
     );
   }
---
+++ src/components/ExperienceItem.tsx
@@ -0,0 +1,82 @@
+import { Component } from "react";
+import "@/components/ExperienceItem.css";
+
+type Props = {
+  badgeColor: string;
+  company?: string;
+  companyLink?: string;
+  employementDescription: string;
+  endDate: string;
+  jobDescription?: string;
+  jobHighlights?: Array<string>;
+  jobTitle: string;
+  location: string;
+  skills?: Array<string>;
+  startDate: string;
+};
+
+class ExperienceItem extends Component<Props> {
+  renderLocation() {
+    if (this.props.company && this.props.companyLink) {
+      return (
+        <i>
+          <a
+            className="company-link"
+            href={this.props.companyLink}
+            target="_blank"
+          >
+            {this.props.company}, {this.props.location}
+          </a>
+        </i>
+      );
+    }
+    return <i>{this.props.location}</i>;
+  }
+  renderSkills() {
+    if (this.props.skills) {
+      return (
+        <div className="skills-used">
+          <i>Skills: </i>{this.props.skills.join(" | ")}
+        </div>
+      );
+    }
+    return "";
+  }
+  renderJobHighlights() {
+    if (this.props.jobHighlights) {
+      return this.props.jobHighlights.map((o) => <li key={o}>{o}</li>);
+    }
+    return "";
+  }
+  renderJobDescription() {
+    if (this.props.jobDescription) {
+      return (
+        <div className="job-description">
+          {this.props.jobDescription}
+          <ul className="tasks-description">{this.renderJobHighlights()}</ul>
+        </div>
+      );
+    }
+    return "";
+  }
+  render() {
+    return (
+      <div className="experience-item">
+        <div className="left-column">
+          {this.props.startDate} - {this.props.endDate}
+        </div>
+        <div className="right-column">
+          <strong>{this.props.jobTitle}</strong>
+          <span className={`badge badge-${this.props.badgeColor}`}>
+            {this.props.employementDescription}
+          </span>
+          <br />
+          {this.renderLocation()}
+          {this.renderJobDescription()}
+          {this.renderSkills()}
+        </div>
+      </div>
+    );
+  }
+}
+export default ExperienceItem;
